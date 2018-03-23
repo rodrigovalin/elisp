15fa87c26529d862de2c83704becd009a675ee72
Rodrigo Valin <licorna@gmail.com>
2018-03-23 22:47:11 +0000

    Fixed shell-command-on-region wrong params.

--- kube.el
+++ kube.el
@@ -4,10 +4,20 @@
 ;;
 ;;; Commentary:
 ;;
+;; This is a simple module I created when working with Kubernetes.
+;; It allows you to apply or delete a buffer or region quickly without
+;; having to visit the command line (which is probably on another
+;; screen). I found it useful for quickly changing your Kubernetes
+;; resources while editing them.
 ;;
+;; Author: Rodrigo Valin <licorna@gmail.com>
 ;;
 ;;; Code:
 
+
+(defun kube--command-on-region (command)
+  "Run COMMAND on region."
+  (shell-command-on-region nil nil command))
 
 (defun kube-delete ()
   "Run kubectl delete -f <this-buffer-filename>."
@@ -15,20 +25,10 @@
   (let ((filename (buffer-file-name)))
     (message (shell-command-to-string (concat "kubectl delete -f " filename)))))
 
-
-(defun kube--command-on-region (command)
-  "Run COMMAND on region."
-  (shell-command-on-region (region-beginning) (region-end) command))
-
 (defun kube-delete-region ()
   "Run kubectl delete -f on region."
   (interactive)
   (kube--command-on-region "kubectl delete -f -"))
-
-(defun kube-apply-region ()
-  "Run kubectl apply -f on region."
-  (interactive)
-  (kube--command-on-region "kubectl apply -f -"))
 
 (defun kube-apply ()
   "Apply this file, creating or updating a given Kubernetes Resource."
@@ -36,6 +36,10 @@
   (let ((filename (buffer-file-name)))
     (message (shell-command-to-string (concat "kubectl apply -f " filename)))))
 
+(defun kube-apply-region ()
+  "Run kubectl apply -f on region."
+  (interactive)
+  (kube--command-on-region "kubectl apply -f -"))
 
 (defun kube-dash ()
   "Open the minikube dashboard."

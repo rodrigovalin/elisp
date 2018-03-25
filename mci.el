5c36c872008dca0fae4a95bb9206584ead28b0dc
Rodrigo Valin <licorna@gmail.com>
2018-03-25 23:39:26 +0100

    Improved helm rows.

--- mci.el
+++ mci.el
@@ -6,39 +6,83 @@
 ;;
 ;; Author: Rodrigo Valin
 ;;
+;;
+;; List of displayed items (rows):
+;;
+;; status : host (host-type) : instance-id : distro : creation : expiration
 
 ;;; code:
 
 
 (defun mci-get-hosts ()
   "Gets mci list of hosts from mci."
-  (mapcar 'json-read-from-string (butlast (split-string (shell-command-to-string "mci plain") "\n"))))
+  (reverse (mapcar 'json-read-from-string (butlast (split-string (shell-command-to-string "mci plain") "\n")))))
 
 
-(defun host-name-from-host (host)
+(defun mci--propertize-status (status)
+  "Return a propertized STATUS depending on contents."
+  (cond ((string-prefix-p "running" status) (propertize status 'face 'bold))
+        ((string-prefix-p "starting" status) (propertize status 'face 'bold))
+        (t status)))
+
+
+(defun /d (date part)
+  "Return for a DATE object, a given PART."
+  (cond ((equal part 'year) (nth 0 date))
+        ((equal part 'month) (nth 1 date))
+        ((equal part 'day) (nth 2 date))
+        ((equal part 'hour) (nth 3 date))
+        ((equal part 'minute) (nth 4 date))
+        ((equal part 'second) (nth 5 date))
+        (t 0)))
+
+
+(defun mci--propertize-date (date)
+  "Return a propertized DATE."
+  (let ((pd (mci--parse-iso8601-datetime date)))
+    (format "%d/%d/%d_%d:%d" (/d pd 'year) (/d pd 'month) (/d pd 'day) (/d pd 'hour) (/d pd 'minute))))
+
+(defun mci--parse-iso8601-datetime (dtime)
+  "Parse an iso8601 DTIME string.  Return a list of YYYY MM DD HH mm SS."
+  (let* ((parted (split-string dtime "T"))
+         (date-part (split-string (car parted) "-"))
+         (time-part (split-string (nth 1 parted) ":")))
+    (mapcar #'string-to-number
+            (list (car date-part) (nth 1 date-part) (nth 2 date-part)
+                  (car time-part) (nth 1 time-part) (car (split-string (nth 2 time-part) "[^0-9]"))))))
+
+
+(defun current-date-iso8601 ()
+  "Return current date in iso8601 format."
+  (concat
+   (format-time-string "%Y-%m-%dT%T")
+   ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
+    (format-time-string "%z"))))
+
+
+(defun current-date ()
+  "Return current date in YYYY MM DD HH mm SS list."
+  (mci--parse-iso8601-datetime (current-date-iso8601)))
+
+
+(defun mci--helm-item (host)
   "Return a string repr of HOST."
-  (concat (alist-get 'host host) ":" (alist-get 'id host) ":" (alist-get 'status host)))
+  (concat
+   (mci--propertize-status (format "%-9s" (alist-get 'status host)))
+   (propertize (format "%-50s" (alist-get 'host host)) 'face 'italic)
+   (format " %-5s " (alist-get 'host_type host))
+   (format "%-20s "(alist-get 'id host))
+   (format "%-20s "(alist-get 'distro host))
+   (mci--propertize-date (alist-get 'creation_time host))
+   " -- "
+   (mci--propertize-date (alist-get 'expiration_time host))))
 
-
-(defun format-candidate-list (host)
-  "Builds a line for HOST to be displayed in results."
-  (require 'helm-grep)
-  (let* ((split (helm-grep-split-line (host-name-from-host host)))
-         (h (car split))
-         (id (nth 1 split))
-         (status (nth 2 split)))
-    (cons (concat (propertize host 'face 'italics)
-                  (propertize ":" 'invisible t)
-                  (propertize id 'face 'italics)
-                  (propertize ":" 'invisible t)
-                  (propertize status 'face 'bold))
-          host)))
 
 (defun build-helm-candidates (hosts)
   "Builds an alist of mci HOSTS."
   (let (ahosts)
     (dolist (host hosts)
-      (add-to-list 'ahosts (cons (host-name-from-host host) host)))
+      (add-to-list 'ahosts (cons (mci--helm-item host) host)))
     ahosts))
 
 
